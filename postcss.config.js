1bb599ad50e9066583cb4c9864f0d087d2d2ff12
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-08-14 13:14:09 +0300

    postcss confg

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,25 @@
 module.exports = () => ({
   plugins: {
-    'stylelint': {},
     'postcss-smart-import': {},
+    'postcss-nested': {},
+    'postcss-flexbugs-fixes': {},
     'postcss-cssnext': {
       features: {
         autoprefixer: true,
+        customProperties: {
+          variables: {
+            mainColor: 'violet',
+          },
+        },
+        customMedia: {
+          extensions: {
+            'xs-only': 'only screen and (max-width: 575px)',
+            'sm-only': 'only screen and (min-width: 576px) and (max-width: 1024px)',
+            'md-only': 'only screen and (min-width: 1025px) and (max-width: 1440px)',
+            'lg-only': 'only screen and (min-width: 1441px)',
+          },
+        },
       },
     },
-    'postcss-nested': {},
-    'postcss-flexbugs-fixes': {},
   },
 });
