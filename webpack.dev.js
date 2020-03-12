642eec2d69e6a4938293e1e27fe8236ba324cb4f
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2020-03-12 11:37:24 +0200

    add link

--- webpack.dev.js
+++ webpack.dev.js
@@ -139,3 +139,4 @@ module.exports = {
 // https://github.com/jantimon/html-webpack-plugin/issues/218
 // https://extri.co/2017/07/11/generating-multiple-html-pages-with-htmlwebpackplugin/
 // https://github.com/ivarprudnikov/webpack-static-html-pages/tree/master/src
+// https://github.com/erickzhao/static-html-webpack-boilerplate
