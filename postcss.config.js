da703e5d5a32398fc10b59abb4bc2a7e9444a1dd
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2020-09-17 17:19:28 +0300

    browserslist

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,6 @@ module.exports = () => ({
     cssnano: {},
     'postcss-preset-env': {
       stage: 3,
-      browsers: ['last 5 versions', '> 5%'],
       features: {
         'custom-media-queries': true,
       },
