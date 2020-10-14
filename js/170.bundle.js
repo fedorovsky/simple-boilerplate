487679c3234712ef31f56ce8657321709282af71
fedorovsky <anton.fedorovsky@gmail.com>
2020-10-14 15:49:38 +0000

    Deploying to gh-pages from  @ a67a95652d193086077fa6a754b4269e6bbece95 🚀

---
+++ js/170.bundle.js
@@ -0,0 +1,1 @@
+(self.webpackChunksimple_layout_boilerplate=self.webpackChunksimple_layout_boilerplate||[]).push([[170],{170:(e,l,t)=>{"use strict";t.r(l),t.d(l,{sum:()=>u,default:()=>s});var u=function(e,l){return e+l};const s={}}}]);
