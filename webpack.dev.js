f5228027d113a8861192b641a50ea1da6f41a679
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2023-09-03 21:48:18 +0200

    added watchFiles

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,11 +5,11 @@ module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'development',
   devServer: {
+    watchFiles: ['src/**/*'],
     open: true,
   },
 });
 
 // https://github.com/jantimon/html-webpack-plugin/issues/218
-// https://extri.co/2017/07/11/generating-multiple-html-pages-with-htmlwebpackplugin/
 // https://github.com/ivarprudnikov/webpack-static-html-pages/tree/master/src
 // https://github.com/erickzhao/static-html-webpack-boilerplate
